4bc4ee5a8c79c3b27ea2d0a26c7b86dd494064ad
Khushi-201 <90816848+Khushi-201@users.noreply.github.com>
2021-09-29 10:46:58 +0530

    Add files via upload

---
+++ Practical04/Practical04_Task03.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+namespace first{
+int add(int a,int b)
+{
+cout<<"Sum :"<<a+b<<endl;
+}}
+namespace second{
+float add(float a,float b)
+{
+cout<<"Sum :"<<a+b<<endl;
+}}
+//using namespace first;
+//using namespace second;
+int main()
+{
+cout<<add(1,2);
+cout<<add(1.1,1.2);
+cout<<add(1.2,2);
+cout<<add(1,2.1);
+cout<<add(0,0);
+return 0;
+}
