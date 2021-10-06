ff27a34df5ad6416a360e348d8d6d37748ae00f5
Khushi-201 <90816848+Khushi-201@users.noreply.github.com>
2021-10-06 10:39:30 +0530

    Add files via upload

---
+++ Practical-06/Practical06_Task1(a).cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+int a=5,b=6;
+float c=6.7,d=8.2;
+string str1="hello",str2="world";
+
+cout<<"int a+int b="<<a+b<<endl;
+cout<<"float c +float d="<<c+d<<endl;
+cout<<"float c+ int a="<<c+a<<endl;
+string str3=str1 + str2;
+cout<<"string str1 + string str2="<<str3;
+
+return 0;
+}
+
