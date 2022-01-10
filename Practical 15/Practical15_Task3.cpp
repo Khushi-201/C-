7ce128a401e4529ac8e537af4fe3975edcee25d2
Khushi-201 <90816848+Khushi-201@users.noreply.github.com>
2022-01-10 21:25:56 +0530

    Create Practical15_Task3.cpp

---
+++ Practical 15/Practical15_Task3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+      cout<<"\t\t Khushi Sharma 200111288 A "<<"\n" ;
+      int n ;
+	//dec hex Manipulators
+	cout<<"Using dec and hex Manipulators  "<<"\n" ;
+	cout<<"Enter a Integer : ";
+	cin>>n ;
+	cout<<"dec  : "<<dec<<n <<"\n" ;
+	cout<<"hex  : "<<hex<<n <<"\n" ;
+	//skipws and no skipws Manipulators
+	char a , b , c ;
+	cout<<"\nUsing Skipws "<<"\n" ;
+	istringstream s("   123") ;
+	s>>skipws>>a>>b>>c ;
+    cout<<a<<b<<c <<endl ;
+    
+	cout<<"\nUsing Noskipws "<<"\n" ;
+	istringstream p("  123") ;
+	p>>noskipws>>a>>b>>c ;
+    cout<<a<<b<<c <<"\n" ;
+    
+    //Use of endl , ends , flush
+    cout<<"\nUsing Endl, ends , flush  "<<"\n" ;
+    cout<<"ABCD, ";
+    cout<<"EFGH "<<endl ;
+    cout<<"IJK "<<ends ;
+    cout<<"LMN "<<flush ;
+    cout<<"\n" ;
+    //setw and setprecision
+    cout<<"\nUsing setprecision , setw  "<<"\n" ;
+    double f = 3.14159 ;
+    cout<<setprecision(5) <<f <<endl ;
+    cout<<setw(8)<<f <<endl ;
+    return 0;
+}
