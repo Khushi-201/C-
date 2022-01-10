9329952409ed6625f72bd19d00a767c67183365a
Khushi-201 <90816848+Khushi-201@users.noreply.github.com>
2022-01-10 21:22:24 +0530

    Create Practical15_Task2_a_.cpp

---
+++ Practical 15/Practical15_Task2_a_.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+    fstream myfile;
+    myfile.open("Sample.txt",ios::in);;
+    if(!myfile)						//if Sample.txt doesn't exists
+{     
+      cout<<"\nFile Not Exist's\n";
+    	exit (0);					//program exit}
+    char data;
+    while(1)
+    {  
+      myfile>>data;				//reading char by char
+        if(myfile.eof())			//till we doesn't encounter end of file
+            break;
+        else cout<<data<<"_";
+    }
+    myfile.close();
+    return 0;
+}
